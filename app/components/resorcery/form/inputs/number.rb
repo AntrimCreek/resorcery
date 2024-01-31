9d0f2162dcf9eded3eb650166ea4420c1ff3529c
Philip Kirkham <pkirkham@antrimcreek.com>
2024-01-31 08:21:31 -0500

    Assign a default step value for number inputs

--- app/components/resorcery/form/inputs/number.rb
+++ app/components/resorcery/form/inputs/number.rb
@@ -2,6 +2,13 @@ module Resorcery
   module Form
     module Inputs
       class Number < BaseInput
+        def initialize(form, attribute, **options)
+          unless options.dig(:input_html, :step).present?
+            options[:input_html] ||= {}
+            options[:input_html][:step] = 10.0**-(form.object.class.columns_hash[attribute.to_s].try(:scale) || 0)
+          end
+          super(form, attribute, **options)
+        end
       end
     end
   end
