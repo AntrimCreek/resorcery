225190eaf9892c84d4e0f569cd62520abecde388
Philip Kirkham <pkirkham@antrimcreek.com>
2025-05-20 08:14:33 -0400

    Add include_blank option to for select inputs

--- app/components/resorcery/form/inputs/select.rb
+++ app/components/resorcery/form/inputs/select.rb
@@ -7,6 +7,11 @@ module Resorcery
         def initialize(form, attribute, collection: [], **options)
           super(form, attribute, **options)
           @collection = collection
+          if options[:include_blank] == true
+            @collection.unshift(["", ""])
+          elsif options[:include_blank].present?
+            @collection.unshift([options[:include_blank].to_s, ""])
+          end
         end
 
         def self.html_default_options
